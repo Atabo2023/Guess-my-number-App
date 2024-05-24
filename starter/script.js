70b81e417ff85f8ed09edc17f3e7c532ee83a515
Atabo2023 <onuchevictor.dev.2023@gmail.com>
2024-05-24 07:45:55 +0100

    debuged

--- starter/script.js
+++ starter/script.js
@@ -132,15 +132,15 @@ document.querySelector(".again").addEventListener("click", function () {
 
 //  #practice solution; //#
 
-(1).document.querySelector("again").addEventListener("click", function () {
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random) * 20 + 1;
 });
 
 displayMessage("start guessing...");
-document.querySelector("number").textContent = "?";
-document.querySelector("score").textContent = "score";
-document.querySelector("guess").textContent = "";
+document.querySelector(".number").textContent = "?";
+document.querySelector(".score").textContent = "score";
+document.querySelector(".guess").value = "";
 
 document.querySelector("body").style.backgroundColor = "#222";
 document.querySelector(".number").style.width = "15rem";
